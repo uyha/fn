f2332822fec7391b5f50f8c2d59eaec09f69e734
Uy Ha <hchanuy@gmail.com>
2020-10-06 00:19:33 +0200

    Make sure that the example code actually runs

---
+++ examples/example.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <fn/fn.hpp>
+
+void function() {}
+
+struct A {
+  int function(){};
+
+  int a{0};
+};
+
+int main() {
+  auto func = river::fn<&function>{};
+  static_assert(river::fn_trait<decltype(&function)>::is_free_fn);
+  func();
+
+  auto mem_func = river::fn<&A::function>{};
+  static_assert(river::fn_trait<decltype(&A::function)>::is_member_fn);
+  A a{};
+  mem_func(a);
+
+  A const b{};
+  auto overloading_func = river::overloading_fn<&A::function>{};
+  overloading_func(a);
+  overloading_func(A{});
+
+  auto member = river::overloading_fn<&A::a>{};
+  assert(member(a) == 0);
+  assert(member(b) == 0);
+  assert(member(A{}) == 0);
+}
