509791770d8f63eeabd101163cb51a95a42c04e0
Uy Ha <hchanuy@gmail.com>
2020-10-17 00:11:36 +0200

    Remove ampersand(&)

--- tests/free_functions.test.cpp
+++ tests/free_functions.test.cpp
@@ -8,20 +8,20 @@
 #include <river/fn.hpp>
 
 TEST_CASE("fn of free function pointers can be called with the correct arguments") {
-  CHECK(river::fn<&fn1>{}(0) == 1);
-  CHECK(river::fn<&fn_noexcept>{}(0, 0) == 1);
+  CHECK(river::fn<fn1>{}(0) == 1);
+  CHECK(river::fn<fn_noexcept>{}(0, 0) == 1);
 
-  CHECK(river::overloading_fn<&fn1>{}(0) == 1);
-  CHECK(river::overloading_fn<&fn_noexcept>{}(0, 0) == 1);
+  CHECK(river::overloading_fn<fn1>{}(0) == 1);
+  CHECK(river::overloading_fn<fn_noexcept>{}(0, 0) == 1);
 }
 
 TEST_CASE("noexcept should be propagated") {
-  CHECK(noexcept(river::fn<&fn_noexcept>{}(0, 0)));
-  CHECK_FALSE(noexcept(river::fn<&fn1>{}(0)));
-  CHECK(noexcept(river::overloading_fn<&fn_noexcept>{}(0, 0)));
-  CHECK_FALSE(noexcept(river::overloading_fn<&fn1>{}(0)));
+  CHECK(noexcept(river::fn<fn_noexcept>{}(0, 0)));
+  CHECK_FALSE(noexcept(river::fn<fn1>{}(0)));
+  CHECK(noexcept(river::overloading_fn<fn_noexcept>{}(0, 0)));
+  CHECK_FALSE(noexcept(river::overloading_fn<fn1>{}(0)));
 }
 
 TEST_CASE("fn of two different function pointers are different types") {
-  CHECK_FALSE(std::is_same_v<river::fn<&fn1>, river::fn<&fn2>>);
+  CHECK_FALSE(std::is_same_v<river::fn<fn1>, river::fn<fn2>>);
 }
